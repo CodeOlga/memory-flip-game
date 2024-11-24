78f3bafb889b1552117057b3a3870aa624807519
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-11-24 12:34:50 +0200

    fix cards

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Card = ({ card, selectionHandler, disabled, opened }) => {
   return (
     <div
-      className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-2xl bg-gradient-to-r from-[#EEEEEE] to-[#393E46] p-4 shadow-2xl shadow-[#393E46] duration-300 hover:shadow-xl hover:shadow-[#393E46]"
+      className="flex aspect-square min-h-[60px] min-w-[60px] cursor-pointer flex-col items-center justify-center rounded-2xl bg-gradient-to-r from-[#EEEEEE] to-[#393E46] p-4 shadow-2xl shadow-[#393E46] duration-300 hover:shadow-xl hover:shadow-[#393E46]"
       onClick={() => {
         if (!disabled && !opened) {
           selectionHandler(card);
@@ -11,36 +11,25 @@ const Card = ({ card, selectionHandler, disabled, opened }) => {
       }}
     >
       {/*<img*/}
-      {/*  className={`${opened ? "" : "[transform:rotateY(-90deg)]"} absolute duration-300`}*/}
-      {/*  src={card.imgUrl}*/}
-      {/*  alt={card.label}*/}
-      {/*  width={60}*/}
-      {/*/>*/}
-
-      {/*<img*/}
-      {/*  className={`absolute duration-300 ${opened ? "" : "[transform:rotateY(-90deg)]"} max-h-full max-w-full object-contain`}*/}
+      {/*  className={`absolute duration-300 ${opened ? "" : "[transform:rotateY(-90deg)]"} h-10 w-10 sm:h-8 sm:w-8 md:h-10 md:w-10 lg:h-12 lg:w-12`}*/}
       {/*  src={card.imgUrl}*/}
       {/*  alt={card.label}*/}
       {/*  width={60}*/}
       {/*/>*/}
 
       <img
-        className={`absolute duration-300 ${opened ? "" : "[transform:rotateY(-90deg)]"} h-10 w-10 sm:h-8 sm:w-8 md:h-10 md:w-10 lg:h-12 lg:w-12`}
+        className={`absolute duration-300 ${
+          opened ? "" : "[transform:rotateY(-90deg)]"
+        } h-10 w-10 sm:h-8 sm:w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 [@media(max-width:375px)]:h-7 [@media(max-width:375px)]:w-7`}
         src={card.imgUrl}
         alt={card.label}
         width={60}
       />
-
-      {/*<img*/}
-      {/*  className={`absolute duration-300 ${opened ? "" : "[transform:rotateY(-90deg)]"} h-10 w-10 sm:h-8 sm:w-8`}*/}
-      {/*  src={card.imgUrl}*/}
-      {/*  alt={card.label}*/}
-      {/*/>*/}
     </div>
   );
 };
 
-// Добавление PropTypes
+// Додавання PropTypes
 Card.propTypes = {
   card: PropTypes.shape({
     imgUrl: PropTypes.string.isRequired,
